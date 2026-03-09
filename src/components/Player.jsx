60726b13b2afd73f720a368a956d8aef25537964
Raghul32 <raghul4032@gmail.com>
2026-03-09 19:40:56 +0530

    Update Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function Player({ initialName, symbol, isActive, onChangeName}) {
+function Player({ initialName, symbol, isActive, onChangeName }) {
 
     const [isEditing, setIsEditing] = useState(false);
     const [playerName, setPlayerName] = useState(initialName);
@@ -11,6 +11,7 @@ function Player({ initialName, symbol, isActive, onChangeName}) {
             onChangeName(symbol, playerName);
         }
     }
+    
 
     function handleChange(event) {
         setPlayerName(event.target.value);
